94ad9c077ac9dfbd8d89b1505b71c885f3d36edb
Gergely Marosvölgyi <marosvolgyi.gergely@gmail.com>
2019-04-13 19:22:53 +0200

    Initial commit

---
+++ trunk/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Marosvölgyi Gergely hivatalos oldala</title>
+
+    <meta charset="utf-8"/>
+    <meta keywords="Marosvölgyi Gergely, vers, versek, novella, novellák"/>
+    <meta description="Marosvölgyi Gergely hivatalos oldala. Versek, novellák"/>
+
+    <link rel="stylesheet" href="css/site.css">
+</head>
+<body>
+    <header>
+    </header>
+    
+    <main>
+    </main>
+
+    <footer>
+        &copy;2019 Marosvölgyi Gergely
+    </footer>
+</body>
+</html>
